4bb9fea047567681527e8613bc4e382bc0bdeb2d
Minhuz Taznim Himel <minhuztaznim@gmail.com>
2025-08-20 11:33:23 +0600

    Add Linux command references for moving, copying, deleting files and directories, and renaming files to 8_cmd.sh

---
+++ 8_cmd.sh
@@ -0,0 +1,23 @@
+(ii) How do you move a file to a different directory in Linux? 
+mv source_file destination_directory
+Example: mv file.txt /home/user/documents/
+
+
+(iii) How do you copy a file to a different directory in Linux? 
+cp source_file destination_directory
+Example: cp file.txt /home/user/documents/
+
+
+(iv) How do you delete a file in Linux? 
+rm file_name
+Example: rm file.txt
+
+
+(v) How do you delete a directory in Linux?
+rmdir directory_name
+Example: rmdir old_directory
+
+
+(vi) How do you rename a file in Linux?
+mv old_file_name new_file_name
+Example: mv oldfile.txt newfile.txt

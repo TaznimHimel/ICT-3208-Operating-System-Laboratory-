c9a052b5835440a0bceaf9b2012aa38f99ebbc02
Minhuz Taznim Himel <minhuztaznim@gmail.com>
2025-08-20 11:20:43 +0600

    Add Linux command reference for file permissions and deletion to 4_cmd.sh

---
+++ 4_cmd.sh
@@ -0,0 +1,16 @@
+(ii) How do you give read, write, and execute permissions to a file or 
+directory in Linux? 
+sudo chmod +rwx <filename>
+Example: sudo chmod +rwx myfile.txt
+
+
+(iii) How do you remove read, write, and execute permissions from a file 
+or directory in Linux? 
+sudo chmod -rwx <filename>
+Example: sudo chmod -rwx myfile.txt
+
+
+(iv) How do you delete a file using the rm command in Linux? 
+sudo rm <filename>
+Example: sudo rm myfile.txt
+

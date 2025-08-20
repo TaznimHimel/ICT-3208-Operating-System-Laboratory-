c1c3cac6d5803465f2814ccadb8119d471b11c7a
Minhuz Taznim Himel <minhuztaznim@gmail.com>
2025-08-20 11:23:19 +0600

    Add Linux command references for file display, copying, creation, and IP address display to 5_cmd.sh

---
+++ 5_cmd.sh
@@ -0,0 +1,26 @@
+(ii) How do you display the contents of a file using the cat command in 
+Linux? 
+sudo cat <filename>
+Example: sudo cat myfile.txt
+
+
+(iii) How do you copy a file from one location to another using the cp 
+command in Linux? 
+sudo cp <source> <destination>
+Example: sudo cp myfile.txt /home/user/documents/
+
+
+(iv) How do you create a new file in Linux? 
+sudo touch <filename>
+Example: sudo touch newfile.txt
+
+
+(v) How do you display the contents of a file on the screen in Linux? 
+sudo less <filename>
+Example: sudo less myfile.txt
+
+
+(vi) How do you display the IP address of the machine in Linux?
+sudo ip addr show
+Example: sudo ip addr show eth0
+

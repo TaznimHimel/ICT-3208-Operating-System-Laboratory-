925aff76c1a99fab41a10f3f662538431c7c3556
Minhuz Taznim Himel <minhuztaznim@gmail.com>
2025-08-20 11:08:01 +0600

    Add comprehensive Linux command reference to 1_cmd.sh for system management tasks

---
+++ 1_cmd.sh
@@ -0,0 +1,98 @@
+(ii) How do you update the system packages in Linux? 
+sudo apt update && sudo apt upgrade
+
+(iii) How do you install a package in Linux? 
+sudo apt install <package_name>
+
+(iv) How do you remove a package in Linux? 
+sudo apt remove <package_name>
+
+(v) How do you display the manual page for a command in Linux? 
+man <command_name>
+
+(vi) How do you display the list of installed packages in Linux?
+dpkg -l
+
+(vii) How do you search for a package in Linux?
+apt search <package_name>
+
+(viii) How do you display the disk usage of files and directories in Linux?
+du -h
+
+(ix) How do you display the current working directory in Linux?
+pwd
+
+(x) How do you change the current working directory in Linux?
+cd <directory_path>
+
+(xi) How do you create a new directory in Linux?
+mkdir <directory_name>
+
+(xii) How do you remove a directory in Linux?
+rmdir <directory_name>
+
+(xiii) How do you copy a file in Linux?
+cp <source_file> <destination_file>
+
+(xiv) How do you move a file in Linux?
+mv <source_file> <destination_file>
+
+(xv) How do you display the contents of a file in Linux?
+cat <file_name>
+
+(xvi) How do you display the first few lines of a file in Linux?
+head <file_name>
+
+(xvii) How do you display the last few lines of a file in Linux?
+tail <file_name>
+
+(xviii) How do you search for a string in a file in Linux?
+grep <string> <file_name>
+
+(xix) How do you display the current date and time in Linux?
+date
+
+(xx) How do you display the system uptime in Linux?
+uptime
+
+(xxi) How do you display the system hostname in Linux?
+hostname
+
+(xii) How do you display the system's IP address in Linux?
+ip addr show
+
+(xiii) How do you display the system's memory usage in Linux?
+free -h
+
+(xiv) How do you display the system's CPU usage in Linux?
+top
+
+(xv) How do you display the system's disk usage in Linux?
+df -h
+
+(xvi) How do you display the system's network interfaces in Linux?
+ifconfig
+
+(xvii) How do you display the system's kernel version in Linux?
+uname -r
+
+(xviii) How do you display the system's architecture in Linux?
+uname -m
+
+(xix) How do you display the list of running processes in Linux?
+ps aux
+
+(xx) How do you display the system's load average in Linux?
+uptime
+
+(xxi) How do you display the system's logged-in users in Linux?
+who
+
+(xii) How do you display the system's last login information in Linux?
+last
+
+(xiii) How do you display the system's current users in Linux?
+whoami
+
+(xiv) How do you display the system's environment variables in Linux?
+printenv

c6083953fe4cdfda365aaf2644a02fd83f6055e2
Minhuz Taznim Himel <minhuztaznim@gmail.com>
2025-08-20 11:33:17 +0600

    Add Linux command references for date, working directory, user account management, and current user display to 7_cmd.sh

---
+++ 7_cmd.sh
@@ -0,0 +1,18 @@
+(ii) How do you display the current date and time in Linux? 
+date 
+
+
+(iii) How do you display the current working directory in Linux?
+pwd
+
+(iv) How do you create a new user account in Linux? 
+useradd username
+Example: useradd newuser
+
+(v) How do you change the password for a user account in Linux? 
+passwd username
+Example: passwd newuser
+
+
+(vi) How do you display the current user name in Linux?
+whoami

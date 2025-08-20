f1003b167e98598233852de28d4101d0537d1a46
Minhuz Taznim Himel <minhuztaznim@gmail.com>
2025-08-20 11:28:12 +0600

    Add Linux command references for file listing, directory creation, file permissions, package removal, and file content display to 6_cmd.sh

---
+++ 6_cmd.sh
@@ -0,0 +1,22 @@
+(ii) How do you list all the files in the current directory in Linux? 
+pwd
+
+(iii) How do you create a new directory in Linux? 
+mkdir new_directory
+Example: mkdir my_folder
+
+(iv) How do you change file permissions in Linux? 
+chmod 644 filename
+Example: chmod 644 myfile.txt
+you are setting the permissions of myfile.txt to:
+Owner → rw- → can read and write
+Group → r-- → can only read
+Others → r-- → can only read
+
+(v) How do you remove a package in Linux?
+sudo apt remove package_name
+Example: sudo apt remove vim
+
+(vi) How do you display the contents of a file in Linux?
+cat filename
+Example: cat myfile.txt
